e4276af1d4b5453fbb098b8c90ed92be34cafb6b
Benjamin Louis <louisbenetval@orange.fr>
2024-01-03 18:19:39 +0100

    maj

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+//   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+//     document.getElementById("wrapper").style.top = "-150px";
+//   } else {
+//     document.getElementById("wrapper").style.top = "0";
+//   }
+
+//   if (document.body.scrollBottom > 0 || document.documentElement.scrollBottom > 0) {
+//     document.getElementById("wrapper").style.top = "0";
+//   }else {
+//     document.getElementById("wrapper").style.top = "-150px";
+//   }
+}

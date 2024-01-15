559ca8f9663d35181dbf292600428923bc7c0e43
Benjamin Louis <louisbenetval@orange.fr>
2024-01-15 09:12:57 +0100

    maj

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  if (document.body.scrollTop > 1170 || document.documentElement.scrollTop > 1170) {
     document.getElementById("wrapper").style.top = "-150px";
   } else {
     document.getElementById("wrapper").style.top = "0";
